1ba1a53b53c62fc65f6a02da5a39738bf3e51a24
Trygve Lie <github@trygve-lie.com>
2015-06-12 14:39:56 +0200

    Better undefined and null checks. More tests

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,4 @@
 /* jshint node: true, strict: true */
-/* global describe: true, it: true, before: true */
 
 "use strict";
 
@@ -7,7 +6,6 @@ var fs      = require('fs'),
     path    = require('path'),
     tap     = require('tap'),
     utils   = require('../lib/utils.js');
-
 
 
 
@@ -19,7 +17,6 @@ tap.test('utils.isRetweet() - object is a retweet', function (t) {
     });
 });
 
-
 tap.test('utils.isRetweet() - object not a retweet', function (t) {
     fs.readFile(path.resolve(__dirname, 'mock.tweet.json'), 'utf-8', function (error, data) {
         var obj = JSON.parse(data);
@@ -27,7 +24,6 @@ tap.test('utils.isRetweet() - object not a retweet', function (t) {
         t.end();
     });
 });
-
 
 tap.test('utils.isRetweet() - object is a delete message', function (t) {
     fs.readFile(path.resolve(__dirname, 'mock.delete.json'), 'utf-8', function (error, data) {
@@ -38,19 +34,35 @@ tap.test('utils.isRetweet() - object is a delete message', function (t) {
 });
 
 
-tap.test('utils.isReply() - object is a delete message', function (t) {
-    fs.readFile(path.resolve(__dirname, 'mock.delete.json'), 'utf-8', function (error, data) {
+
+tap.test('utils.isReply() - object is a reply to a user message', function (t) {
+    fs.readFile(path.resolve(__dirname, 'mock.reply.to.user.json'), 'utf-8', function (error, data) {
+        var obj = JSON.parse(data);
+        t.equal(true, utils.isReply(obj), 'is true');
+        t.end();
+    });
+});
+
+tap.test('utils.isReply() - object is a reply to the user itself', function (t) {
+    fs.readFile(path.resolve(__dirname, 'mock.reply.to.self.json'), 'utf-8', function (error, data) {
         var obj = JSON.parse(data);
         t.equal(false, utils.isReply(obj), 'is false');
         t.end();
     });
 });
 
-
-tap.test('utils.isReply() - object is a reply to a user message', function (t) {
-    fs.readFile(path.resolve(__dirname, 'mock.reply.to.user.json'), 'utf-8', function (error, data) {
+tap.test('utils.isReply() - object is not a reply', function (t) {
+    fs.readFile(path.resolve(__dirname, 'mock.tweet.json'), 'utf-8', function (error, data) {
         var obj = JSON.parse(data);
-        t.equal(true, utils.isReply(obj), 'is false');
+        t.equal(false, utils.isReply(obj), 'is false');
+        t.end();
+    });
+});
+
+tap.test('utils.isReply() - object is a delete message', function (t) {
+    fs.readFile(path.resolve(__dirname, 'mock.delete.json'), 'utf-8', function (error, data) {
+        var obj = JSON.parse(data);
+        t.equal(false, utils.isReply(obj), 'is false');
         t.end();
     });
 });
